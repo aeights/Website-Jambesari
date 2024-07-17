fe3a6fcee77e275294a60fe864ea8f1bacdd73a3
aeights <m.afifma.ruf8@gmail.com>
2024-07-17 20:23:21 +0700

    feat: adding template for landing page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Landing\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/','index')->name('landing');
 });
